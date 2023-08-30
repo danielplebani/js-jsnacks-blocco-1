5b91ae8942e17e15c827190560443dbbdff0ab1d
danielplebani <danielpleba03@gmail.com>
2023-08-30 10:59:50 +0200

    finish

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -11,12 +11,15 @@ tools:
 */
 
 //chiedo all'utente due numeri con 2 prompt
+/*
 const number1 = Number(prompt('inserisci un numero a tua scelta'));
 const number2 = Number(prompt('inserisci un numero a tua scelta'));
 
 console.log(number1, number2);
-
+*/
 //controllo qual è il numero maggiore e lo stampo in pagina
+
+/*
 if (number1 > number2) {
     console.log(number1);
     document.getElementById('highestNumber').innerHTML = 'il numero più alto è il: ' + number1;
@@ -27,3 +30,33 @@ if (number1 > number2) {
     console.log('pareggio');
     document.getElementById('highestNumber').innerHTML = 'i numeri scelti sono uguali,quindi è pareggio';
 }
+*/
+
+//2
+let n = 0;
+
+let numberList = [];
+
+while (n < 2) {
+    const number1 = Number(prompt('inserisci un numero a tua scelta'));
+
+    if (isNaN(number1)) {
+        alert('inserisci un  numero!');
+        n--;
+    } else {
+        numberList.push(number1);
+        n++
+    }
+}
+
+if (numberList[0] > numberList [1]) {
+    console.log(numberList[0]);
+    document.getElementById('highestNumber').innerHTML = 'il numero più alto è il: ' + numberList[0];
+} else if (numberList[0] < numberList [1]) {
+    console.log(numberList[1]);
+    document.getElementById('highestNumber').innerHTML = 'il numero più alto è il: ' + numberList [1];
+} else {
+    console.log('pareggio');
+    document.getElementById('highestNumber').innerHTML = 'pareggio';
+}
+    
