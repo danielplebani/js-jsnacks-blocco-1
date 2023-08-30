01165e65dcc45a870f4a117c1fcf4c2e178d7d3d
danielplebani <danielpleba03@gmail.com>
2023-08-30 12:53:16 +0200

    jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -12,30 +12,48 @@ tools:
 
 //creo un ciclo che chiede dieci volte un numero tramite un prompt
 /*
+let somma = 0;
+
 for (let i = 0; i < 10; i++) {
-    const element = prompt('scegli un numero a tua scelta!');
+    const element = Number(prompt('scegli un numero a tua scelta!'));
     console.log(element);
+
+    if (isNaN(element)) {
+        alert('inserisci un  numero!');
+    }
     //collego la costante alla unorder list e appendo una lista creata
     const ulEl = document.querySelector('ul');
     const liEl = document.createElement('li');
     liEl.append(element);
 
     ulEl.append(liEl);
+
+    somma += element;
+    console.log('la somma totale è: ' + somma);
+    document.querySelector('span').innerHTML = 'la somma totale è: ' + somma;
 }
 */
+
 
 //2
 let n = 0;
 
-numberList = [];
+let numberList = [];
 
-somma = 
+let somma = 0;
 
 while (n < 10) {
     const number1 = Number(prompt('inserisci un numero!'))
     console.log(number1);
 
-    numberList.push(number1);
+    if (isNaN(number1)) {
+        alert('inserisci un  numero!');
+    } else {
+        numberList.push(number1);
+        n++
+    }
 
-    n++
+    somma += number1;
+    console.log('la somma totale è: ' + somma);
+    document.querySelector('span').innerHTML = 'la somma totale è: ' + somma;
 }
